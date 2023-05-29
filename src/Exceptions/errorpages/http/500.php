5139358d710092da7ef50c2ab0d624b93403ec97
Noah Wilderom <wilderomnoah@gmail.com>
2023-05-29 20:24:44 +0200

    wip

---
+++ src/Exceptions/errorpages/http/500.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo config('app.name') ?></title>
+    <style>
+        body {
+            background: #000124;
+            text-align: center;
+        }
+
+        h1 {
+            font-size: 65px;
+            color: #fff;
+            display: flex;
+            text-align: center;
+            justify-content: center;
+        }
+
+        p {
+            font-size: 20px;
+            font-weight: 600;
+            color: lightgrey;
+        }
+
+        #root {
+            margin-top: 10%;
+        }
+    </style>
+</head>
+
+<body>
+<div id="root">
+    <h1>
+        500
+    </h1>
+
+    <p>
+        <?=
+        $error
+        ?>
+    </p>
+</div>
+
+</body>
+
+</html>
